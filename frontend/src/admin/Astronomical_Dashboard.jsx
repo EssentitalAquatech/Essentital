ae02530344f806ec1d33d483479a6f1d2b910ce2
Abhishek <ytabhishek721@gmail.com>
2026-01-20 18:44:56 +0530

    Updates

--- frontend/src/admin/Astronomical_Dashboard.jsx
+++ frontend/src/admin/Astronomical_Dashboard.jsx
@@ -20,6 +20,41 @@
 
 
 
+// import React from "react";
+
+// function Astronomical() {
+//   return (
+//     <div style={{ width: "100%", height: "100vh" }}>
+//       <iframe
+//         src="https://d2vy9m5vkf9fe2.cloudfront.net/"
+//         title="Astronomical Dashboard"
+//         style={{
+//           width: "100%",
+//           height: "100%",
+//           border: "none",
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+// export default Astronomical;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from "react";
 
 function Astronomical() {
@@ -33,6 +68,8 @@ function Astronomical() {
           height: "100%",
           border: "none",
         }}
+        allow="geolocation"
+        allowFullScreen
       />
     </div>
   );
