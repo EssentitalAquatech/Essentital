216c526b90643ce3a5cefc25a2ba25684a0f6804
Abhishek <ytabhishek721@gmail.com>
2026-02-03 16:08:14 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -85,10 +85,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-// ===== START SERVER =====
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
 
 
 
