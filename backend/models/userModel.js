5ab502a624a6568dd98173507f2de32762d65e4b
Abhishek <ytabhishek721@gmail.com>
2026-02-12 13:13:40 +0530

    update

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -35,6 +35,53 @@
 
 
 
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: String,
+//     mobile: String,
+//     email: { type: String, unique: true },
+//     age: Number,
+//     address: String,
+//     role: { type: String, default: "agent" },
+
+//     profilePic: { data: Buffer, contentType: String },
+//     aadharFront: { data: Buffer, contentType: String },
+//     aadharBack: { data: Buffer, contentType: String },
+//     panCard: { data: Buffer, contentType: String },
+//     savingAccountImage: { data: Buffer, contentType: String },
+
+//     accountNumber: String,
+//     ifsc: String,
+//     password: String,
+
+//     // ✅ ADD THESE
+//     emailOtp: String,
+//     emailOtpExpiry: Date,
+//   },
+//   { timestamps: true }
+// );
+
+// export default mongoose.model("User", userSchema);
+
+
+
+//ye vala sahi hai selfie ko add karne vala
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
@@ -56,15 +103,15 @@ const userSchema = new mongoose.Schema(
     ifsc: String,
     password: String,
 
-    // ✅ ADD THESE
+    // Signup OTP
     emailOtp: String,
     emailOtpExpiry: Date,
+
+    // Forgot Password OTP
+    forgotPasswordOtp: String,
+    forgotPasswordOtpExpiry: Number,
   },
   { timestamps: true }
 );
 
 export default mongoose.model("User", userSchema);
-
-
-
-//ye vala sahi hai selfie ko add karne vala
